474d675526d25221c218d1c0723246127007f7dd
NeverSettels <settels.eric@gmail.com>
2020-04-03 09:47:05 -0700

    added branching to the correct place

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 let popArray = (name, number) => {
   let numArr = []
+  let resultArr = []
   for (let i = 0; i <= number; i++) {
     numArr.push(i);
   }
-  // numArr.forEach(num=>{
-
-  // })
-
-} 
+  numArr.forEach(num => {
+    let temp = num.toString().split('');
+    if (temp.includes('3')) resultArr.push(`Won't you be my neighbor, ${name}?`);
+    else if (temp.includes('2')) resultArr.push('Boop!,');
+    else if (temp.includes('1')) resultArr.push('Beep!,');
+    else resultArr.push(`${num},`);
+  })
+  return resultArr.join(' ')
+}
+console.log(popArray('eric', 5));
